4897800565fe8789c7116dd6325e7e9f78a99e10
Andrew Rich <andrew.rich@lge.com>
2014-12-30 20:18:35 -0800

    Update version string to 2.5.4-pre.1.lite.1

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 (function (enyo, scope) {
 
 	if (enyo && enyo.version) {
-		enyo.version['enyo-webos'] = "2.5.3-pre.4.lite.1";
+		enyo.version['enyo-webos'] = "2.5.4-pre.1.lite.1";
 	}
 
 })(enyo, this);
