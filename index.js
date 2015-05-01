208a4cee2d7ff64cc1db1bce243b0bb1a21ca98a
Jason Robitaille <robitaille88@gmail.com>
2015-05-01 04:05:57 -0500

    Removed unneeded system check. Unknown systems now are labelled webOS.platform.unknown

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ var
 var
 	Signals = require('enyo/Signals');
 
-if (platform.webos || global.PalmSystem) {
-	// doesn't keep it from being loaded into build but does keep it from executing the module
-	require('./assets/webOSjs-0.1.0/webOS');
-	if (!global.cordova) {
-		// if Cordova not used, add signal generation for the "menubutton" event used
-		// in webOS.js for legacy webOS and Open webOS for the appmenu
-		document.addEventListener('menubutton', utils.bind(Signals, 'send', 'onmenubutton'), false);
-	}
-} else logger.warn('webOS.js not loaded: Current platform not supported.');
+require('./assets/webOSjs-0.1.0/webOS');
+if (!global.cordova) {
+	// if Cordova not used, add signal generation for the "menubutton" event used
+	// in webOS.js for legacy webOS and Open webOS for the appmenu
+	document.addEventListener('menubutton', utils.bind(Signals, 'send', 'onmenubutton'), false);
+}
 
 exports.version = '2.6.0-pre.5.dev';
