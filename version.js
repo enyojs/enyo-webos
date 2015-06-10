e73a22e18ef0454da2c0175868bb36cd21f54849
Jenkins <jenkins@gecko.palm.com>
2015-06-09 18:21:35 -0700

    Update version string to 2.5.3-pre.22.wb

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 (function (enyo, scope) {
 
 	if (enyo && enyo.version) {
-		enyo.version['enyo-webos'] = "2.5.3-pre.20.wb";
+		enyo.version['enyo-webos'] = "2.5.3-pre.22.wb";
 	}
 
 })(enyo, this);
