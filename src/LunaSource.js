390a353e961de4a406b6a08b38e45fbfc87805ef
Stephen Choi <stephen.choi@lge.com>
2016-02-24 17:13:10 -0800

    ENYO-1998: Fix removeRequest condition to let cancel remove request instance
    Enyo-DCO-1.1-Signed-off-by: Stephen Choi <stephen.choi@lge.com>

--- src/LunaSource.js
+++ src/LunaSource.js
@@ -107,12 +107,12 @@ module.exports = kind(
 
 	removeRequest: function (req) {
 		var i;
-		if (req.subscribe && !req.resubscribe) {
+		if (req.subscribe) {
 			i = this.activeSubscriptionRequests.indexOf(req);
 			if (i !== -1) {
 				this.activeSubscriptionRequests.splice(i, 1);
 			}
-		} else if (!req.subscribe) {
+		} else {
 			i = this.activeRequests.indexOf(req);
 			if (i !== -1) {
 				this.activeRequests.splice(i, 1);
@@ -135,7 +135,9 @@ module.exports = kind(
 			opts.error(error, req);
 		}
 
-		this.removeRequest(req);
+		if (!req.resubscribe) {
+			this.removeRequest(req);
+		}
 	},
 
 	/**
