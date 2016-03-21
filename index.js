9ef13c05115ccb07672c468bf388e7d65a73002a
Jason Robitaille <jason.robitaille@lge.com>
2016-03-21 11:20:17 -0700

    Update version string to 2.7.0-rc.2

--- index.js
+++ index.js
@@ -18,4 +18,4 @@ if (!global.cordova) {
 dispatcher.listen(document, 'webOSMouse');
 dispatcher.listen(document, 'keyboardStateChange');
 
-exports.version = '2.7.0-rc.1';
+exports.version = '2.7.0-rc.2';
