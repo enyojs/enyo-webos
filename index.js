0db1157b9cc45c713fa9886960d00561ece0060e
Jason Robitaille <jason.robitaille@lge.com>
2016-06-23 01:42:47 -0700

    Update version string to 2.6.4-rc.4.1

--- index.js
+++ index.js
@@ -18,4 +18,4 @@ if (!global.cordova) {
 dispatcher.listen(document, 'webOSMouse');
 dispatcher.listen(document, 'keyboardStateChange');
 
-exports.version = '2.6.4-rc.4';
+exports.version = '2.6.4-rc.4.1';
